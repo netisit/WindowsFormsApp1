65b669caf34dbde120d62c89ee8607b4d8ab00e1
Netisit <34535451+Netisit@users.noreply.github.com>
2018-11-22 11:34:29 +0900

    First Commit

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,10 +20,6 @@ namespace WindowsFormsApp1
         private void label1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("test");
-            MessageBox.Show("test");
-            MessageBox.Show("test");
-            MessageBox.Show("test");
-            MessageBox.Show("test");
         }
     }
 }
